6000f4ec6f8726fcac41c70175e242bbeff2c285
Antony Holmes <antony.b.holmes@gmail.com>
2018-09-20 14:19:21 -0400

    Updates and bug fixes.

--- api/seq/urls.py
+++ api/seq/urls.py
@@ -7,5 +7,6 @@ from api.seq import views
 urlpatterns = [
     path('chipseq/', include('api.seq.chipseq.urls')),
     path('counts', views.counts, name='counts'),
+    path('mapped', views.mapped, name='mapped'),
     path('type', views.data_type, name='type'),
 ]
