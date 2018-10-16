3172affbed612eff60cdfa90de19f5b6abc37493
Antony Holmes <antony.b.holmes@gmail.com>
2018-10-16 18:56:49 -0400

    Updates and bug fixes.

--- api/seq/views.py
+++ api/seq/views.py
@@ -9,7 +9,7 @@ from edbw import settings
 
 import re
 import sys
-
+import struct
 import os
 import libseq
 import libdna
@@ -22,19 +22,19 @@ from api.vfs.models import VFSFile
 
 def counts_callback(key, person, user_type, id_map=None):
     if 'id' not in id_map:
-        return JsonResponse(['id'], safe=False)
+        return JsonResponse([], safe=False)
         
     id = id_map['id'][0]
     
     if 'g' not in id_map:
-        return JsonResponse(['g', ';'.join(sorted(id_map))], safe=False)
+        return JsonResponse([], safe=False)
     
     genome = id_map['g'][0]
     
     loc = libhttpdna.get_loc_from_params(id_map)
     
     if loc is None:
-        return JsonResponse(['loc'], safe=False)
+        return JsonResponse([], safe=False)
         
     
     # Get the path location
@@ -58,7 +58,7 @@ def counts_callback(key, person, user_type, id_map=None):
     bcr = libseq.BinCountReader(dir, genome=genome)
     locations = bcr.get_counts(loc, bin_width=bin_width)
     
-    return JsonResponse([{'id' : id, 'l' : loc.__str__(), 'bw' : bin_width, 'c' : locations.tolist()}], safe=False)    
+    return JsonResponse([{'id' : id, 'l' : loc.__str__(), 'bw' : bin_width, 'c' : locations}], safe=False)    
 
 
 def counts(request):
@@ -78,21 +78,36 @@ def mapped_callback(key, person, user_type, id_map={}):
     
     # Get the path location
      
-    #sub_dirs = VFSFile.objects.filter(id=11244) #samplefile__sample__=id)
     sub_dirs = VFSFile.objects.filter(samplefile__sample=id)
     
     if len(sub_dirs) == 0:
         return JsonResponse([], safe=False)
         
+    genome = id_map['g'][0]
+    
+    power = id_map['p'][0]
+    
+    if power == -1:
+        bin_width = id_map['bw'][0]
+        
+        if bin_width in libseq.POWER:
+            power = libseq.POWER[bin_width]
+            
+    if power == -1:
+        return JsonResponse([], safe=False)
+        
     sub_dir = sub_dirs[0].path
         
-    file = settings.DATA_DIR + sub_dir + '/mapped_reads_count.txt' #os.path.join(settings.SEQ_DIR, sub_dir) #str(id))
+    file = settings.DATA_DIR + sub_dir + '/counts.{}.{}bw.bc'.format(genome, power) #'/mapped_reads_count.txt' #os.path.join(settings.SEQ_DIR, sub_dir) #str(id))
     
     
     
     if os.path.isfile(file):
-        f = open(file, 'r')
-        count = int(f.readline().strip())
+        #f = open(file, 'r')
+        #count = int(f.readline().strip())
+        #f.close()
+        f = open(file, 'rb')
+        count = struct.unpack('<I', f.read(4)) #int(f.readline().strip())
         f.close()
     else:
         count = 0
@@ -103,12 +118,7 @@ def mapped_callback(key, person, user_type, id_map={}):
 
 
 def mapped(request):
-    id_map = {}
-    
-    #auth.parse_ids(request, {'bw' : 100}, id_map=id_map)
-    libhttp.parse_params(request, {'id':-1}, id_map=id_map)
-    
-    #return counts_callback(None, None, None, id_map=id_map)
+    id_map = libhttp.parse_params(request, {'id':-1, 'p':-1, 'bw':-1, 'g':'grch38'}, id_map=id_map)
     
     return auth.auth(request, mapped_callback, id_map=id_map)
         
