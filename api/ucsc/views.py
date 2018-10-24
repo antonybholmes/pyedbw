04dfaf2c1d255cd0000ef33678fda1fb2e28c502
Antony Holmes <antony.b.holmes@gmail.com>
2018-10-24 15:45:58 -0400

    Updates and bug fixes.

--- api/ucsc/views.py
+++ api/ucsc/views.py
@@ -10,15 +10,25 @@ from api import auth
   
 def tracks_callback(key, person, user_type, id_map={}):
     ids = id_map['id']
-
+    mode = id_map['mode'][0]
+    
     tracks = Track.objects.filter(sample_id__in=ids) #Track.objects.all() #(id__in=ids)
     
-    serializer = TrackSerializer(tracks, many=True, read_only=True)
     
-    return JsonResponse(serializer.data, safe=False)    
-
+    if (mode == 'json'):
+        serializer = TrackSerializer(tracks, many=True, read_only=True)
+    
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        output = []
+        
+        for track in tracks:
+            l = 'track type=bigWig name="{}" description="{}" visibility="full" bigDataUrl={}'.format(track.name, track.name, track.url)
+            output.append(l)
+            
+        return HttpResponse("\n".join(output), content_type='text/plain')
 
 def tracks(request):
-    id_map = libhttp.parse_params(request, {'id':-1, 'key':''})
+    id_map = libhttp.parse_params(request, {'id':-1, 'key':'', 'mode':'text'})
  
     return auth.auth(request, tracks_callback, id_map=id_map)
