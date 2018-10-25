c7f8b6e2a37f6feea097300b2213effb92ae8ca2
Antony Holmes <antony.b.holmes@gmail.com>
2018-10-25 19:28:01 -0400

    Updates and bug fixes.

--- api/ucsc/views.py
+++ api/ucsc/views.py
@@ -9,17 +9,20 @@ from api import auth
 
 def hex_to_rgb(value):
     value = value.lstrip('#')
-    return tuple(int(value[i:(i + 2)], 16) for i in range(0, 6, 2))
+    #return tuple(int(value[i:(i + 2)], 16) for i in range(0, 6, 2))
+    return ','.join([str(int(value[i:(i + 2)], 16)) for i in range(0, 6, 2)])
     
   
 def tracks_callback(key, person, user_type, id_map={}):
     ids = id_map['id']
-    colors = [hex_to_rgb(c) for c in id_map['color']]
+    colors = [hex_to_rgb(c) for c in id_map['c']]
+    
+    while len(colors) < len(ids):
+        colors.append(colors[0])
     
     mode = id_map['mode'][0]
     
     tracks = Track.objects.filter(sample_id__in=ids) #Track.objects.all() #(id__in=ids)
-    
     
     if (mode == 'json'):
         serializer = TrackSerializer(tracks, many=True, read_only=True)
@@ -31,14 +34,14 @@ def tracks_callback(key, person, user_type, id_map={}):
         for i in range(0, len(ids)):
             track = tracks[i]
             color = colors[i]
-            
+           
             name = track.sample.name
-            l = 'track type=bigWig name="{}" description="{}" visibility="full" color={}, bigDataUrl={}'.format(name, name, ','.join(color), track.url)
+            l = 'track type=bigWig name="{}" description="{}" visibility="full" color={}, bigDataUrl={}'.format(name, name, color, track.url)
             output.append(l)
             
         return HttpResponse("\n".join(output), content_type='text/plain')
 
 def tracks(request):
-    id_map = libhttp.parse_params(request, {'id':-1, 'key':'', 'mode':'text', 'color':'#FF0000'})
+    id_map = libhttp.parse_params(request, {'id':-1, 'key':'', 'mode':'text', 'c':'#FF0000'})
  
     return auth.auth(request, tracks_callback, id_map=id_map)
