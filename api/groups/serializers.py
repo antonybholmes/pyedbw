35032006e96c1914b48b293bc9fc3a6220354c39
Antony Holmes <antony.b.holmes@gmail.com>
2020-01-07 18:36:05 -0500

    Updates and bug fixes.

--- api/groups/serializers.py
+++ api/groups/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from api.groups.models import Group
+from django.contrib.auth.models import Group
 
 class GroupSerializer(serializers.ModelSerializer):
     n = serializers.CharField(source='name')
-    c = serializers.CharField(source='color')
+    #c = serializers.CharField(source='color')
 
     class Meta:
         model = Group
-        fields = ('id', 'n', 'c')
+        fields = ('id', 'n')
--- api/groups/views.py
+++ api/groups/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
 from django.core.paginator import Paginator
-from api.groups.models import Group
+from django.contrib.auth.models import Group
 from api.groups.serializers import GroupSerializer
 from api import auth, views
 from edbw import settings
@@ -19,18 +19,25 @@ def groups_callback(key, person, user_type, id_map={}):
     if 'page' in id_map:
         page = id_map['page']
     else:
-        page = -1
+        page = 1
     
     records = min(id_map['records'], settings.MAX_RECORDS_PER_PAGE)
     
-    rows = Group.objects.all().values('json')
+    rows = Group.objects.all() #.values('json')
     
     paginator = Paginator(rows, records)
-     
-    if page > 0:
-        return views.json_page_resp('groups', page, paginator) #JsonResponse({'page':page, 'pages':paginator.num_pages, 'groups':[x['json'] for x in page_rows]}, safe=False)
+    
+    page_rows = paginator.get_page(1)
+    
+    data = []
+    
+    for g in page_rows:
+        data.append({'id':g.id, 'n':g.name, 'c':'#000000'})
+    
+    if 'page' in id_map:
+        return JsonResponse({'page':page, 'pages':paginator.num_pages, 'groups':data, 'size':len(data)}, safe=False) #views.json_page_resp('groups', page, paginator) #JsonResponse({'page':page, 'pages':paginator.num_pages, 'groups':[x['json'] for x in page_rows]}, safe=False)
     else:
-        return views.json_resp(paginator.get_page(1))
+        return JsonResponse(data, safe=False) #views.json_resp(paginator.get_page(1))
     
     
 def groups(request):
